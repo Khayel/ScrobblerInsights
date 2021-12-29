e20c5a30b5ce7d6592e7b67854a7fbbd28161d36
Kyle Chua <kylechua6514@gmail.com>
2021-12-29 10:28:53 -0700

    remove old comments

--- web/src/components/Timeline.js
+++ web/src/components/Timeline.js
@@ -77,9 +77,6 @@ function Timeline() {
          setIsLoaded(true);
          setSortby('year')
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           console.log(error)
           setIsLoaded(true);
