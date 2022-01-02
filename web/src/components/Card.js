928d6fb496a9e1d17e4f2af68aa81413cd364180
Kyle Chua <kylechua6514@gmail.com>
2022-01-01 23:02:17 -0700

    loading icon, only set to my user.

--- web/src/components/Card.js
+++ web/src/components/Card.js
@@ -1,18 +1,21 @@
-import React, {useState} from "react"
+import React, {useState,useEffect} from "react"
 import {IoChevronDown, IoChevronUp} from "react-icons/io5"
 
 function Card(props){
     const [displayMode, setDisplayMode] = useState('minimized')
     const [clickedinCard, setClicked] = useState(false)
 
+
     const expandCard = (e)=>{
         e.preventDefault();
+        if (props.cardType !== "cardYear"){
         if (!clickedinCard){setDisplayMode('expanded')}
-    }
+    }}
     const minimizeCard = (e) =>{
         e.preventDefault();
+        if (props.cardType !== "cardYear"){
         if (!clickedinCard){setDisplayMode('minimized')}
-    }
+    }}
     const maximizeCard = (e) =>{
         e.preventDefault();
         setClicked( (prevState) => !prevState);
@@ -21,15 +24,21 @@ function Card(props){
 
 
     let displayIcon = <IoChevronUp/>
+
+    useEffect( ()=> {
+        if (props.cardType === "cardYear"){
+            setDisplayMode('expanded')
+        }
+    }, [])
     if(displayMode ==='expanded'){ displayIcon = <IoChevronDown/>}else{ displayIcon = <IoChevronUp/>}
     
         return (
-            <div className={(displayMode === 'minimized') ? "card card-default" : "card card-" + displayMode} onMouseEnter={expandCard} onMouseLeave={minimizeCard} onClick={maximizeCard} >
+            <div className={(displayMode === 'minimized') ? `card card-default ${props.cardType}`  : `card card-${displayMode} ${props.cardType}`} onMouseEnter={expandCard} onMouseLeave={minimizeCard} onClick={maximizeCard} >
                 <div className="upper">
                 <span className="timestamp">{props.timestamp}</span>{displayIcon}
                 </div>
                 <div className="lower">
-                <span className={"content " +  displayMode}>
+                <span className={`content ${displayMode}`}>
                     {props.content}
                      </span><br></br>
                 </div>
--- web/src/components/Timeline.js
+++ web/src/components/Timeline.js
@@ -13,75 +13,72 @@ function Timeline() {
  // useEffect( () =>{ aggTrackList()},[sortby]);
   
   
-  const aggTrackList = () => {
-    console.log("sortby changed" + isLoaded)
-    //TODO define method to isolate unique days/months/years
-    //TODO for day
-    //  { "dayValue": [ [0:00am, Let it be, The Beatles],[23:00, Song,Artist]
-    //  {}
-    setSortedList([])
-    let sortedObj = {};
-    switch(sortby){
-      case 'day':
-        trackList.forEach( (track) => {
-           let dayVal = new Date(track[3]).toDateString();
-           (dayVal in sortedObj) ? sortedObj[dayVal].push(track) : sortedObj[dayVal] = [track];
+  // const aggTrackList = () => {
+  //   console.log("sortby changed" + isLoaded)
+  //   setSortedList([])
+  //   let sortedObj = {};
+  //   switch(sortby){
+  //     case 'day':
+  //       trackList.forEach( (track) => {
+  //          let dayVal = new Date(track[3]).toDateString();
+  //          (dayVal in sortedObj) ? sortedObj[dayVal].push(track) : sortedObj[dayVal] = [track];
     
-            }
-           )
-           setSortedList(sortedObj);
-           console.log(sortedList)
-           break;
-        //   //isolate appropriate date string in this case day and create a list of tracks...
+  //           }
+  //          )
+  //          setSortedList(sortedObj);
+  //          console.log(sortedList)
+  //          break;
+  //       //   //isolate appropriate date string in this case day and create a list of tracks...
           
-        //sort tracklist by day
-      case 'week':
-        //sort tracklist by month
-      case 'month':
-        trackList.forEach( (track) => {
-          let monthVal = new Date(track[3]).toDateString().split(' ');
-          let monthYear = monthVal[1] + monthVal[3];
-          (monthYear in sortedObj) ? sortedObj[monthYear].push(track) : sortedObj[monthYear] = [track];
+  //       //sort tracklist by day
+  //     case 'week':
+  //       //sort tracklist by month
+  //     case 'month':
+  //       trackList.forEach( (track) => {
+  //         let monthVal = new Date(track[3]).toDateString().split(' ');
+  //         let monthYear = monthVal[1] + monthVal[3];
+  //         (monthYear in sortedObj) ? sortedObj[monthYear].push(track) : sortedObj[monthYear] = [track];
           
-        });
+  //       });
         
-        setSortedList(sortedObj)
-        break;
-        //sort...
-      case 'year':
-        trackList.forEach( (track) => {
-          let yearVal = new Date(track[3]).toDateString().split(' ');
-          let year = yearVal[3];
-          (year in sortedObj) ? sortedObj[year].push(track) : sortedObj[year] = [track];
+  //       setSortedList(sortedObj)
+  //       break;
+  //       //sort...
+  //     case 'year':
+  //       trackList.forEach( (track) => {
+  //         let yearVal = new Date(track[3]).toDateString().split(' ');
+  //         let year = yearVal[3];
+  //         (year in sortedObj) ? sortedObj[year].push(track) : sortedObj[year] = [track];
           
-        })
-        setSortedList(sortedObj)
-        break;
-        //sort
-    }
-    console.log("finishyed sorted" + isLoaded)
-  }
+  //       })
+  //       setSortedList(sortedObj)
+  //       break;
+  //       //sort
+  //   }
+  //   console.log("finishyed sorted" + isLoaded)
+  // }
 
-  const getUserTracks = (username)=>{
+  
+  useEffect(()=>{
     setError(null)
-    fetch(`http://192.168.1.65:5001/user/khayelc`,
-    { headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-     }})
-      .then(res => res.json())
-      .then(
-        (result) => {
-          settracks(result['tracks_played']);
-          // aggTrackList();
-         setIsLoaded(true);
-         setSortby('year')
-        },
-        (error) => {
-          console.log(error)
-          setIsLoaded(true);
-          setError(error);
-        })};
+      fetch(`http://192.168.1.65:5001/user/khayelc`,
+      { headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }})
+        .then(res => res.json())
+        .then(
+          (result) => {
+            settracks(result['tracks_played']);
+            // aggTrackList();
+           setIsLoaded(true);
+           setSortby('year')
+          },
+          (error) => {
+            console.log(error)
+            setIsLoaded(true);
+            setError(error);
+          })},[]);
   
   function organizeCards(){
     console.log("organizing...")
@@ -110,50 +107,25 @@ function Timeline() {
     return sortedObj;
 }
 
-
-  const newCard =  <div className="Timeline">
+return (
+  <div className="Timeline">
   {(!isLoaded) ?
   <div className="usernameForm">
-      <h1>type in your username to get started</h1>
-      <UserForm onSubmit={getUserTracks}/>
+    <div class="lds-facebook"><div></div><div></div><div></div></div>
   </div> :
     [<SortMenu setSort={ (selected) => {setSortby(selected)}}/>,
       <div className="timelineContainer">  
           <div className="trackList">
-            {/* {
-            Object.keys(sortedList).map( (year) => (
-              <div className="year">
-                <Card content={year}/>
-                {Object.keys(sortedList[year]).map( (month) => (
-                <div className="month">
-                  <Card content={month}/>
-                  {Object.keys(sortedList[year][month]).map( (day) => (
-                    <div className="day">
-                    <Card content={day}/>
-                    <ul>
-                    {Object.entries(sortedList[year][month][day]).map( (track) => (
-                      track
-                      ))}
-                    </ul>
-                    </div>
-                  ))}
-                  </div>
-                ))}
-                </div>
-            ))} */}
-
-
-            {/* NESTED CARD ELEMENTS */}
               {
             Object.keys(sortedList).map( (year) => (
-              <div className="year">
-                <Card timestamp={year} content=
+              <>
+                <Card timestamp={year} cardType="cardYear" content=
                 {Object.keys(sortedList[year]).map( (month) => (
                 <div className="month">
-                  <Card timestamp={month} content=
+                  <Card timestamp={month} cardType="cardMonth" content=
                   {Object.keys(sortedList[year][month]).map( (day) => (
                     <div className="day">
-                    <Card timestamp={day} content={
+                    <Card timestamp={day} cardType="cardDay" content={
                     (<ul>
                       
                     {Object.entries(sortedList[year][month][day]).map( (track) => (
@@ -165,47 +137,14 @@ function Timeline() {
                   ))}/>
                   </div>
                 ))}/>
-                </div>
+                </>
             ))}
-          
-            {/* {(isLoaded) ?
-              // Object.entries(sortedList).map( ([timeVal,track]) => (<Card className="card" displayMode={sortby} key={track[4]}  timestamp={timeVal} content={track}/> )):
-               sortedList.map( (item) => (<Card className="card" content={item}/>))
-               :
-               <div></div>
-    } */}
   {(error) ? <div>Error: {error.message}</div>: <div></div> }
     </div>
   </div>
   ]
 }
-</div>;
-
-
-
-
-
-return (newCard
-  //   <div className="Timeline">
-    
-  //       {(!isLoaded) ?
-  //       <div className="usernameForm">
-  //           <h1>type in your username to get started</h1>
-  //           <UserForm onSubmit={getUserTracks}/>
-  //       </div> :
-  //         [<SortMenu setSort={ (selected) => {setSortby(selected)}}/>,
-  //           <div className="timelineContainer">  
-  //               <div className="trackList">
-  //                 {(isLoaded) ?
-  //                   Object.entries(sortedList).map( ([timeVal,track]) => (<Card className="card" displayMode={sortby} key={track[4]}  timestamp={timeVal} content={track}/> )):
-  //                    <div></div>
-  //         }
-  //       {(error) ? <div>Error: {error.message}</div>: <div></div> }
-  //         </div>
-  //       </div>
-  //       ]
-  //     }
-  // </div>
+</div>
   );
 }
 export default Timeline;
